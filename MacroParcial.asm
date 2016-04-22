c83b039b076bf0ea12c70a6ddd8296da0f88a3a9
johan-smc <johan_smc@hotmail.com>
2016-04-21 19:11:02 -0500

    Movimientos

--- MacroParcial.asm
+++ MacroParcial.asm
@@ -1,3 +1,6 @@
+	.eqv NEGRO 0x00000000
+	.eqv COLOR_BORDE 0x00ff0000
+	.text
 .macro puntoColor(%t0,%t1,%t2,%t3)
 	sll $t1,$t1,2
 	sll $t2,$t2,7
@@ -5,7 +8,7 @@
 	add $t1,$t0,$t1
 	sw $t3,0($t1) 
 .end_macro
-
+#t0-inicioPantalla,t1-adonde,t3-Color
 .macro puntoColor(%t0,%t1,%t3)
 	mul $t1,$t1,4
 	add $t1,$t0,$t1
@@ -65,4 +68,48 @@ salir:
 	li $t2,31
 	li $t4,32
 	imprimirLineaV($t0,$t2,$t3,$t4)
+.end_macro
+
+.macro leerDireccion()
+	li $v0,12
+	syscall
+.end_macro
+
+.macro calcularMovimiento(%v0)
+	beq $v0,'d',mRight
+	beq $v0,'a',mLeft
+	beq $v0,'w',mUp
+	beq $v0,'s',mDown
+	li $v0,0
+	j exit
+mRight:
+	li $v0,1
+	j exit
+mLeft:
+	li $v0,-1
+	j exit
+mUp:
+	li $v0,-32
+	j exit
+mDown:
+	li $v0,32
+	j exit
+exit:
+.end_macro
+
+.macro inicioJugador(%t0,%k1,%t3)
+	li $k1,33
+	add $t1,$k1,$zero
+	puntoColor($t0,$t1,$t3)
+.end_macro
+#t0-inicioPantalla,k1-PosicionJugador,v0-movimiento,t3-color
+.macro moverJugador(%t0,%k1,%v0,%t3)
+	add $t2,$t3,$zero
+	li $t3,NEGRO
+	add $t1,$k1,$zero
+	puntoColor($t0,$k1,$t3)
+	add $t3,$t2,$zero
+	add $k1,$k1,$v0
+	add $t1,$k1,$zero
+	puntoColor($t0,$k1,$t3)
 .end_macro
