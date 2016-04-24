558d03222c52af1073da1278f1e529097e34316e
johan-smc <johan_smc@hotmail.com>
2016-04-23 22:33:56 -0500

    juego avance 1.0.0

--- MacroParcial.asm
+++ MacroParcial.asm
@@ -1,5 +1,7 @@
 	.eqv NEGRO 0x00000000
 	.eqv COLOR_BORDE 0x00ff0000
+	.eqv COLOR_JUGADOR 0x00ffffff
+	.eqv COLOR_META 0x00FFFACD
 	.text
 .macro puntoColor(%t0,%t1,%t2,%t3)
 	sll $t1,$t1,2
@@ -102,8 +104,49 @@ exit:
 	add $t1,$k1,$zero
 	puntoColor($t0,$t1,$t3)
 .end_macro
+
+.macro validar(%t0,%t1,%s0,%t3)
+	mul $t1,$t1,4
+	add $t1,$t0,$t1
+	lw  $s0,0($t1)
+	sub $s0,$s0,$t3
+.end_macro
+.macro  menu()
+	li $t0,0
+.end_macro
+.macro jugarMapa1()
+	li $t3,NEGRO
+	borrarPantalla($t0,$t3,$a0)
+	li $t3,COLOR_JUGADOR
+	inicioJugador($t0,$k1,$t3)
+.end_macro
+.macro jugarMapa2()
+	li $t3,COLOR_BORDE
+	borrarPantalla($t0,$t3,$a0)
+.end_macro
+.macro imprimirWinner()
+	li $t3,NEGRO
+	borrarPantalla($t0,$t3,$a0)
+	li $t1,55
+	li $t3,COLOR_BORDE
+	puntoColor($t0,$t1,$t3)
+	menu()
+.end_macro
 #t0-inicioPantalla,k1-PosicionJugador,v0-movimiento,t3-color
-.macro moverJugador(%t0,%k1,%v0,%t3)
+.macro moverJugador(%t0,%k1,%v0,%t3,%t9)
+	add $t1,$k1,$v0
+	add $t4,$zero,$t3
+	li $t3,NEGRO
+	validar($t0,$t1,$s0,$t3)
+	add $t5,$s0,$zero
+	add $t3,$zero,$t4	###BUENA
+	
+	add $t1,$k1,$v0
+	add $t4,$zero,$t3
+	li $t3,COLOR_META
+	validar($t0,$t1,$s0,$t3)
+	add $t3,$zero,$t4	##GANO
+	
 	add $t2,$t3,$zero
 	li $t3,NEGRO
 	add $t1,$k1,$zero
@@ -112,4 +155,29 @@ exit:
 	add $k1,$k1,$v0
 	add $t1,$k1,$zero
 	puntoColor($t0,$k1,$t3)
+	li $a0,4
+	sleep($a0)
+	beq $s0,0,winner
+	bne $t5,0,reiniciar
+	j sal
+reiniciar:
+	beq $t9,0,rMapa1
+	jugarMapa2()
+	j sal
+rMapa1:
+	jugarMapa1()
+	j sal
+winner:
+	imprimirWinner()
+sal:
+.end_macro
+
+.macro jugar()
+re:
+	leerDireccion()
+	calcularMovimiento($v0)
+	li $t9,0
+	li $t3,COLOR_JUGADOR
+	moverJugador($t0,$k1,$v0,$t3,$t9)
+	bne $t0,0,re
 .end_macro
--- main.asm
+++ main.asm
@@ -3,14 +3,22 @@
 	.eqv INICIO_PANTALLA 0xffff0000
 	.eqv NEGRO 0x00000000
 	.eqv COLOR_BORDE 0x00ff0000
-	#.eqv COLOR_JUGADOR
-	#.eqv COLOR_META
+	.eqv COLOR_JUGADOR 0x00ffffff
+	.eqv COLOR_META 0x00FFFACD
 	.text
 main:
+	
 	li $t0,INICIO_PANTALLA
-	li $t3,0x00fffdfd
+	li $t3,COLOR_JUGADOR
 	inicioJugador($t0,$k1,$t3)
-	leerDireccion()
-	calcularMovimiento($v0)
-	moverJugador($t0,$k1,$v0,$t3)
+	li $t0,INICIO_PANTALLA
+	li $t1,1
+	li $t3,COLOR_BORDE
+	puntoColor($t0,$t1,$t3)
+	li $t0,INICIO_PANTALLA
+	li $t1,65
+	li $t3,COLOR_META
+	puntoColor($t0,$t1,$t3)
+	jugar()
+	
 	
